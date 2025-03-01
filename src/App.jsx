b14e9e800cf2220d222fadb336f62217cd9d6882
Billy-Ugalde <billyugalde2004@gmail.com>
2025-03-01 11:45:16 -0600

    dfgfd

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  //Hook
+  //useState is a hook that lets you
+  //add react state to function components
+  const [count, setCount] = useState(1) //estado actual eso es useState(0|1) 
+  //count es un contador que inicia en 0
+//que cuando el contador llegue al 5 imprima un mensaje
+  return (
+
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
